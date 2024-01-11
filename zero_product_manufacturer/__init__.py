304c6825757b5c33893ff014fce8333580f0edb9
Zero Systems <isszero06@gmail.com>
2024-01-11 18:35:07 +0200

    Update __init__.py

--- zero_product_manufacturer/__init__.py
+++ zero_product_manufacturer/__init__.py
@@ -9,15 +9,5 @@
 #
 #################################################################################
 
-from odoo import SUPERUSER_ID, api
 
 from . import models
-
-def pre_init_check_vers(cr):
-    from odoo.service import common
-    from odoo.exceptions import Warning
-    version_info = common.exp_version()
-    server_serie =version_info.get('server_serie')
-    if server_serie!='16.0':
-    	raise Warning('Module support Odoo series 16.0 found {}.'.format(server_serie))
-    return True
